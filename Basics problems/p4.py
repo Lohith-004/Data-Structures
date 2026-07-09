7a1d50008b5f4a483290230aea8b71e48da78c3e
Lohith Gowda C P <lohithgowda062004@gmail.com>
2026-07-09 13:14:17 +0530

    Create p4.py

---
+++ Basics problems/p4.py
@@ -0,0 +1,17 @@
+# Palindrome numbeer
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        num = x
+        result = 0
+
+        while num != 0:
+            digit =  num % 10
+            result = result*10 + digit
+            num = num // 10
+
+        return x == result
+        
